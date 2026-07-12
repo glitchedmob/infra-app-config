277686b1b3c07b6f4fa41f9cdbc6ea5f9055c545
Levi Zitting <me@levizitting.com>
2026-07-12 14:27:02 -0500

    feat: reenable oidc for openbao

--- src/tf/modules/openbao/oidc.tf
+++ src/tf/modules/openbao/oidc.tf
@@ -5,30 +5,30 @@ ephemeral "aws_ssm_parameter" "dex_client_secrets" {
   arn = "arn:aws:ssm:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:parameter/vm-workloads/lz/infra-vm-workloads/dex-client-secrets"
 }
 
-# resource "vault_jwt_auth_backend" "oidc" {
-#   description                   = "OIDC auth via shared Dex (sso.levizitting.com)"
-#   path                          = "oidc"
-#   type                          = "oidc"
-#   oidc_discovery_url            = "https://sso.levizitting.com"
-#   oidc_client_id                = "openbao"
-#   oidc_client_secret_wo         = jsondecode(ephemeral.aws_ssm_parameter.dex_client_secrets.value).openbaoClientSecret
-#   oidc_client_secret_wo_version = 1
-#   default_role                  = "admin"
-#   bound_issuer                  = "https://sso.levizitting.com"
-# }
-#
-# resource "vault_jwt_auth_backend_role" "admin" {
-#   backend    = vault_jwt_auth_backend.oidc.path
-#   role_name  = "admin"
-#   role_type  = "oidc"
-#   user_claim = "email"
-#   bound_claims = {
-#     email = "me@levizitting.com"
-#   }
-#   token_policies = ["default"]
-#   oidc_scopes    = ["openid", "profile", "email"]
-#   allowed_redirect_uris = [
-#     "https://secrets.levizitting.com/ui/vault/auth/oidc/oidc/callback",
-#     "https://secrets.levizitting.com/auth/oidc/callback",
-#   ]
-# }
+resource "vault_jwt_auth_backend" "oidc" {
+  description                   = "OIDC auth via shared Dex (sso.levizitting.com)"
+  path                          = "oidc"
+  type                          = "oidc"
+  oidc_discovery_url            = "https://sso.levizitting.com"
+  oidc_client_id                = "openbao"
+  oidc_client_secret_wo         = jsondecode(ephemeral.aws_ssm_parameter.dex_client_secrets.value).openbaoClientSecret
+  oidc_client_secret_wo_version = 1
+  default_role                  = "admin"
+  bound_issuer                  = "https://sso.levizitting.com"
+}
+
+resource "vault_jwt_auth_backend_role" "admin" {
+  backend    = vault_jwt_auth_backend.oidc.path
+  role_name  = "admin"
+  role_type  = "oidc"
+  user_claim = "email"
+  bound_claims = {
+    email = "me@levizitting.com"
+  }
+  token_policies = ["default"]
+  oidc_scopes    = ["openid", "profile", "email"]
+  allowed_redirect_uris = [
+    "https://secrets.levizitting.com/ui/vault/auth/oidc/oidc/callback",
+    "https://secrets.levizitting.com/auth/oidc/callback",
+  ]
+}
