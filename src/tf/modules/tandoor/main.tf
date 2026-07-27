eb781972ea0356dbc6f680bd1b9db2965a8d2b97
Levi Zitting <me@levizitting.com>
2026-07-27 00:29:46 -0500

    feat: add tandoor application credentials

--- src/tf/modules/tandoor/main.tf
+++ src/tf/modules/tandoor/main.tf
@@ -1,6 +1,32 @@
 locals {
   application_slug = "tandoor"
   application_url  = "https://tandoor.levizitting.com"
+
+  secret_versions = {
+    runtime = 1
+    s3      = 1
+    backup  = 1
+  }
+}
+
+ephemeral "random_password" "runtime_secret_key" {
+  length  = 64
+  special = false
+}
+
+ephemeral "random_password" "s3_access_key" {
+  length  = 24
+  special = false
+}
+
+ephemeral "random_password" "s3_secret_key" {
+  length  = 48
+  special = false
+}
+
+ephemeral "random_password" "restic_password" {
+  length  = 40
+  special = false
 }
 
 data "authentik_flow" "authentication" {
@@ -75,4 +101,35 @@ resource "vault_kv_secret_v2" "oidc" {
     discoveryUrl = "${var.authentik_url}/application/o/${local.application_slug}/.well-known/openid-configuration"
   })
   data_json_wo_version = 1
+}
+
+resource "vault_kv_secret_v2" "runtime" {
+  mount        = var.applications_mount_path
+  name         = "tandoor/runtime"
+  disable_read = true
+  data_json_wo = jsonencode({
+    secretKey = ephemeral.random_password.runtime_secret_key.result
+  })
+  data_json_wo_version = local.secret_versions.runtime
+}
+
+resource "vault_kv_secret_v2" "s3" {
+  mount        = var.applications_mount_path
+  name         = "tandoor/s3"
+  disable_read = true
+  data_json_wo = jsonencode({
+    accessKey = ephemeral.random_password.s3_access_key.result
+    secretKey = ephemeral.random_password.s3_secret_key.result
+  })
+  data_json_wo_version = local.secret_versions.s3
+}
+
+resource "vault_kv_secret_v2" "backup" {
+  mount        = var.applications_mount_path
+  name         = "tandoor/backup"
+  disable_read = true
+  data_json_wo = jsonencode({
+    resticPassword = ephemeral.random_password.restic_password.result
+  })
+  data_json_wo_version = local.secret_versions.backup
 }
