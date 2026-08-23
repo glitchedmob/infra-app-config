ffbe76b016d3b1d5474f7f1b3907db34457417ab
Levi Zitting <me@levizitting.com>
2026-08-22 22:34:16 -0500

    chore: remove applied tandoor state moves

--- src/tf/moved.tf
+++
@@ -1,9 +0,0 @@
-moved {
-  from = module.openbao.vault_policy.tandoor_secrets
-  to   = module.tandoor.vault_policy.secrets
-}
-
-moved {
-  from = module.openbao.vault_kubernetes_auth_backend_role.tandoor_secrets
-  to   = module.tandoor.vault_kubernetes_auth_backend_role.secrets
-}
