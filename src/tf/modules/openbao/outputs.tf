41a9efb37a8035d415cdd2ac4d019d64a1efcb44
Levi Zitting <me@levizitting.com>
2026-08-22 22:30:02 -0500

    refactor: encapsulate tandoor secrets access

--- src/tf/modules/openbao/outputs.tf
+++ src/tf/modules/openbao/outputs.tf
@@ -2,3 +2,8 @@ output "applications_mount_path" {
   description = "Path of the shared application secrets KV mount"
   value       = vault_mount.applications.path
 }
+
+output "kubernetes_auth_path" {
+  description = "Path of the Kubernetes authentication backend"
+  value       = vault_auth_backend.kubernetes.path
+}
