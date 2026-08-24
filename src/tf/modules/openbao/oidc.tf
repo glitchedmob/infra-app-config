8ee9ad0dc1003a33644376b9450a0817d6177069
Levi Zitting <me@levizitting.com>
2026-08-24 08:15:28 -0500

    fix: read OpenBao Dex secret parameter (#6)

--- src/tf/modules/openbao/oidc.tf
+++ src/tf/modules/openbao/oidc.tf
@@ -1,8 +1,8 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
-ephemeral "aws_ssm_parameter" "dex_client_secrets" {
-  arn = "arn:aws:ssm:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:parameter/vm-workloads/lz/infra-vm-workloads/dex-client-secrets"
+ephemeral "aws_ssm_parameter" "dex_openbao_client_secret" {
+  arn = "arn:aws:ssm:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:parameter/vm-workloads/lz/infra-vm-workloads/dex-openbao-client-secret"
 }
 
 resource "vault_jwt_auth_backend" "oidc" {
@@ -11,8 +11,8 @@ resource "vault_jwt_auth_backend" "oidc" {
   type                          = "oidc"
   oidc_discovery_url            = "https://sso.levizitting.com"
   oidc_client_id                = "openbao"
-  oidc_client_secret_wo         = jsondecode(ephemeral.aws_ssm_parameter.dex_client_secrets.value).openbaoClientSecret
-  oidc_client_secret_wo_version = 1
+  oidc_client_secret_wo         = ephemeral.aws_ssm_parameter.dex_openbao_client_secret.value
+  oidc_client_secret_wo_version = 2
   default_role                  = "admin"
   bound_issuer                  = "https://sso.levizitting.com"
 }
